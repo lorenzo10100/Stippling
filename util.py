0ce73dceb1b42940e30fcb2cfd7b338f8b955940
Lorenzo10100 <lorenzo.scalera20@gmail.com>
2023-01-05 16:32:52 +0100

    Debuggato tutto il main, ora funziona

--- util.py
+++ util.py
@@ -7,3 +7,4 @@ def rescaleFloat64s(float_array, new_Min, new_Max):
     rescaled = np.zeros(len(float_array), dtype=np.float64)
     for i, flo in enumerate(float_array):
         rescaled[i] = new_Min + (new_Max - new_Min) * flo/old_Min
+    return rescaled
